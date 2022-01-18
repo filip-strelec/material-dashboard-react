fc057d780f8bdb5b502a61cfa4759841066ce992
Filip Strelec <38225333+filip-strelec@users.noreply.github.com>
2022-01-18 20:44:16 +0100

    Update Admin.js

--- src/layouts/Admin.js
+++ src/layouts/Admin.js
@@ -1,9 +1,6 @@
 import React from "react";
-import axios from 'axios';
-import Button from "components/CustomButtons/Button.js";
-import WindowFocusHandler from '../assets/focusTab'
-import styled from 'styled-components'
-import { Switch, Route, Redirect } from "react-router-dom";
+import WindowFocusHandler from "../assets/focusTab";
+
 // creates a beautiful scrollbar
 import PerfectScrollbar from "perfect-scrollbar";
 import "perfect-scrollbar/css/perfect-scrollbar.css";
@@ -11,9 +8,6 @@ import "perfect-scrollbar/css/perfect-scrollbar.css";
 import { makeStyles } from "@material-ui/core/styles";
 // core components
 import Navbar from "components/Navbars/Navbar.js";
-import Footer from "components/Footer/Footer.js";
-import Sidebar from "components/Sidebar/Sidebar.js";
-import FixedPlugin from "components/FixedPlugin/FixedPlugin.js";
 
 import routes from "routes.js";
 
@@ -24,108 +18,64 @@ import logo from "assets/img/reactlogo.png";
 
 let ps;
 
+function UserGreeting(props) {
+  return <WindowFocusHandler />;
+}
 
-const switchRoutes = (
-  <Switch>
-    {routes.map((prop, key) => {
-      if (prop.layout === "/admin") {
-        return (
-          <Route
-            path={prop.layout + prop.path}
-            component={prop.component}
-            key={key}
-          />
-        );
-      }
-      return null;
-    })}
-    <Redirect from="/admin" to="/admin/dashboard" />
-  </Switch>
-);
+function GuestGreeting(props) {
+  let enteredName = prompt("password");
+
+  while (
+    sessionStorage.getItem("password") !== process.env.REACT_APP_PASSWORD
+  ) {
+    enteredName = prompt("password");
+    sessionStorage.setItem("password", enteredName);
+    sessionStorage.setItem("date", Date.now());
+  }
+
+  console.log(sessionStorage.getItem("password"));
+
+  return <h1>Please sign up.</h1>;
+}
+
+function Greeting(props) {
+  const isLoggedIn = props.isLoggedIn;
+
+  if (isLoggedIn) {
+    return <UserGreeting />;
+  }
+  return <GuestGreeting />;
+}
 
 const useStyles = makeStyles(styles);
 
-export default function Admin({ ...rest }) {
+export default function Admin() {
   // styles
   const classes = useStyles();
   // ref to help us initialize PerfectScrollbar on windows devices
   const mainPanel = React.createRef();
   // states and functions
-  const [image, setImage] = React.useState(bgImage);
-  const [color, setColor] = React.useState("blue");
-  const [fixedClasses, setFixedClasses] = React.useState("dropdown show");
+
   const [mobileOpen, setMobileOpen] = React.useState(false);
-  const handleImageClick = image => {
-    setImage(image);
-  };
-  const handleColorClick = color => {
-    setColor(color);
-  };
-  const handleFixedClick = () => {
-    if (fixedClasses === "dropdown") {
-      setFixedClasses("dropdown show");
-    } else {
-      setFixedClasses("dropdown");
-    }
-  };
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-  const getRoute = () => {
-    return window.location.pathname !== "/admin/maps";
-  };
-  const resizeFunction = () => {
-    if (window.innerWidth >= 960) {
-      setMobileOpen(false);
-    }
-  };
+
+  const [LoggedIn, setLoggedIn] = React.useState(
+    sessionStorage.getItem("password") === process.env.REACT_APP_PASSWORD
+  );
+
   // initialize and destroy the PerfectScrollbar plugin
   React.useEffect(() => {
-    if (navigator.platform.indexOf("Win") > -1) {
-      ps = new PerfectScrollbar(mainPanel.current, {
-        suppressScrollX: true,
-        suppressScrollY: false
-      });
-      document.body.style.overflow = "hidden";
-    }
-    window.addEventListener("resize", resizeFunction);
-    // Specify how to clean up after this effect:
-    return function cleanup() {
-      if (navigator.platform.indexOf("Win") > -1) {
-        ps.destroy();
-      }
-      window.removeEventListener("resize", resizeFunction);
-    };
-  }, [mainPanel]);
-  
- 
+    setLoggedIn(
+      sessionStorage.getItem("password") === process.env.REACT_APP_PASSWORD
+    );
+  }, [LoggedIn]);
 
- 
-
-  
   return (
     <div className={classes.wrapper}>
-    
-      <Sidebar
-        routes={routes}
-        logoText={"Creative Tim"}
-        logo={logo}
-        image={image}
-        handleDrawerToggle={handleDrawerToggle}
-        open={mobileOpen}
-        color={color}
-        {...rest}
-      />
       <div className={classes.mainPanel} ref={mainPanel}>
-        <Navbar
-          routes={routes}
-          handleDrawerToggle={handleDrawerToggle}
-          {...rest}
-        />
         {/* On the /maps route we want the map to be on full screen - this is not possible if the content and conatiner classes are present because they have some paddings which would make the map smaller */}
-       <div>
-       <WindowFocusHandler/>
-       </div>
+        <div>
+          <Greeting isLoggedIn={LoggedIn} />
+        </div>
       </div>
     </div>
   );
