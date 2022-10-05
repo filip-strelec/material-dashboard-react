f0983706b2d0607f55d0eceb1c906207c1e7fc11
Filip Strelec <filip.strelec@gmail.com>
2022-10-05 03:27:30 +0200

    fix

--- src/assets/focusTab.jsx
+++ src/assets/focusTab.jsx
@@ -367,7 +367,7 @@ console.log("FILIP TESTIRAM")
       </StyledButton>
       <Image visibility={clicked ? "1" : "0"} src={loading}></Image>
       <TextContainer>
-        {( heatingTimeout!=0 ) &&(<Text>heating Timeout {new Date(heatingTimeout - new Date().getTime())} </Text>)}
+        {( heatingTimeout!=0 ) &&(<Text>heating Timeout {new Date(heatingTimeout)} </Text>)}
         <Text>Temperatura:{Temp} °C</Text>
         <Text>Vlažnost:{Humidity} %</Text>
         <Text>PM2.5: {Pm25} pcs/0.01cf</Text>
